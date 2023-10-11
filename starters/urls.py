80aeff7432aa6b1a8a7001d3d73ba553805df167
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-10-11 10:22:15 +0000

    Add Crud Functionality to Starters

--- starters/urls.py
+++ starters/urls.py
@@ -1,10 +1,75 @@
 from django.urls import path
-from .views import ViewStarter
+from .views import ViewStarter, StarterDelete
+from . import views
 
 
 urlpatterns = [
     path("",
          ViewStarter.as_view(),
          name="starters"
-         ),]
-
+         ),
+    path('starter/add_starter/',
+         views.add_starter,
+         name='add_starter'
+         ),
+    path('starter_view/<pk>/',
+         views.starter_view,
+         name='starter_view'
+         ),
+    path('edit_starter/<pk>/update/',
+         views.edit_starter,
+         name="edit_starter"
+         ),
+    path('starter_delete/<pk>/delete/',
+         StarterDelete.as_view(),
+         name="starter_delete"
+         ),
+    path('starter_method/<pk>/',
+         views.starter_method,
+         name='starter_method'
+         ),
+    path('starter_ingredients/<pk>/',
+         views.starter_ingredients,
+         name='starter_ingredients'
+         ),
+    path('htmx/add_starter_ing/',
+         views.add_starter_ing,
+         name='add_starter_ing'
+         ),
+    path('htmx/starter_ing_details/<pk>/',
+         views.starter_ing_details,
+         name="starter_ing_details"
+         ),
+    path('starter_ing_detail_view/<pk>/',
+         views.starter_ing_detail_view,
+         name="starter_ing_detail_view"
+         ),
+    path('starter_step_detail_view/<pk>/',
+         views.starter_step_detail_view,
+         name="starter_stepdetail_view"
+         ),
+    path('htmx/update_starter_ing/<pk>/update/',
+         views.update_starter_ing,
+         name="update_starter_ing"
+         ),
+    path('htmx/delete_ingredient/<pk>/delete/',
+         views.delete_starter_ing,
+         name="delete_starter_ing"
+         ),
+    path('htmx/add_starter_step/',
+         views.add_starter_step,
+         name='add_starter_step'
+         ),
+    path('htmx/starter_step_details/<pk>/update/',
+         views.starter_step_details,
+         name="starter_step_details"
+         ),
+    path('htmx/update_starter_step/<pk>/update/',
+         views.update_starter_step,
+         name="update_starter_step"
+         ),
+    path('htmx/delete_starter_step/<pk>/delete/',
+         views.delete_starter_step,
+         name="delete_starter_step"
+         ),
+]
--- starters/views.py
+++ starters/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.http.response import HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import ListView, DeleteView
 from django.db.models import Q
-from .models import Starter
+from django.contrib import messages
+from .forms import StarterIngredientForm, StarterForm, StarterMethodForm
+from .models import Starter, StarterIngredients, StarterMethod
 
 
 class ViewStarter(ListView):
@@ -23,3 +27,292 @@ class ViewStarter(ListView):
         else:
             starter = self.model.objects.all()
         return starter
+
+
+def add_starter(request):
+    """
+    Add starter Course function
+    """
+    form = StarterForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            starter = form.save(commit=False)
+            starter.starter = starter
+            starter.save()
+            return redirect("starter_view", pk=starter.id)
+        else:
+            return render(request,
+                          "starters/add_starter.html",
+                          context={
+                              "form": form
+                              })
+    context = {
+        "form": form,
+    }
+    return render(request, "starters/add_starter.html", context)
+
+
+def starter_view(request, pk):
+    """
+    View full Starter Recipie
+    """
+    starter = Starter.objects.get(id=pk)
+    starter_step = StarterMethod.objects.filter(starter=starter)
+    starter_ingredients = StarterIngredients.objects.filter(starter=starter)
+
+    context = {
+        "starter": starter,
+        "starter_ingredients": starter_ingredients,
+        "starter_step": starter_step
+    }
+
+    return render(request, "starters/starter_view.html", context,)
+
+
+def starter_ingredients(request, pk):
+    """
+    Creates Ingredient Fields And Add More Enterys
+    """
+    starter = Starter.objects.get(id=pk)
+    starter_ingredients = StarterIngredients.objects.filter(starter=starter)
+    starter_step = StarterMethod.objects.filter(starter=starter)
+    form = StarterIngredientForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            starter_ingredient = form.save(commit=False)
+            starter_ingredient.starter = starter
+            starter_ingredient.save()
+            return redirect("starter_ing_details", pk=starter_ingredient.id)
+        else:
+            return render(request,
+                          "includes/add_starter_ing.html",
+                          context={
+                              "form": form
+                              })
+
+    context = {
+        "form": form,
+        "starter": starter,
+        "starter_ingredients": starter_ingredients,
+        "starter_step": starter_step
+    }
+
+    return render(request, "starters/starter_ingredients.html", context)
+
+
+def starter_method(request, pk):
+    """
+    Add Recipe Steps
+    """
+    starter = Starter.objects.get(id=pk)
+    starter_step = StarterMethod.objects.filter(starter=starter)
+    starter_ingredients = StarterIngredients.objects.filter(starter=starter)
+    form = StarterMethodForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            starter_step = form.save(commit=False)
+            starter_step.starter = starter
+            starter_step.save()
+            return redirect("starter_step_details", pk=starter_step.id)
+        else:
+            return render(request,
+                          "includes/add_starter_step.html",
+                          context={
+                              "form": form
+                              })
+
+    context = {
+        "form": form,
+        "starter": starter,
+        "starter_step": starter_step,
+        "starter_ingredients": starter_ingredients
+    }
+
+    return render(request, "starters/starter_method.html", context)
+
+
+def update_starter_ing(request, pk):
+    """
+    Updates Ingredient Fields
+    """
+    starter_ingredient = StarterIngredients.objects.get(id=pk)
+    form = StarterIngredientForm(
+        request.POST or None,
+        instance=starter_ingredient
+        )
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated Successfull!')
+        return redirect("starter_ing_details", pk=starter_ingredient.id)
+
+    context = {
+        "form": form,
+        "starter_ingredient": starter_ingredient
+    }
+
+    return render(request, "includes/add_starter_ing.html", context)
+
+
+def update_starter_step(request, pk):
+    """
+    Updates Ingredient Fields
+    """
+    starter_step = StarterMethod.objects.get(id=pk)
+    form = StarterMethodForm(request.POST or None, instance=starter_step)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated Successfully!')
+        return redirect("starter_step_details", pk=starter_step.id)
+
+    context = {
+        "form": form,
+        "starter_step": starter_step
+    }
+
+    return render(request, "includes/add_starter_ing.html", context)
+
+
+def edit_starter(request, pk):
+    """
+    Updates Ingredient Fields
+    """
+    starter = Starter.objects.get(id=pk)
+    form = StarterForm(request.POST or None, instance=starter)
+    starter_step = StarterMethod.objects.filter(starter=starter)
+    starter_ingredients = StarterIngredients.objects.filter(starter=starter)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated Successfully!')
+        return redirect("starter_view", pk=starter.id)
+
+    context = {
+        "form": form,
+        "starter": starter,
+        "starter_ingredients": starter_ingredients,
+        "starter_step": starter_step,
+    }
+
+    return render(request, "starters/edit_starter.html", context)
+
+
+def delete_starter_ing(request, pk):
+    """
+    Deletes Ingredient Fields
+    """
+    starter_ingredient = get_object_or_404(StarterIngredients, id=pk)
+
+    if request.method == "POST":
+        starter_ingredient.delete()
+        messages.success(request, 'Ingredient Deleted')
+        return HttpResponse("")
+
+    return HttpResponseNotAllowed(
+        [
+            "POST",
+        ]
+    )
+
+
+def delete_starter_step(request, pk):
+    """
+    Deletes Step Fields
+    """
+    starter_step = get_object_or_404(StarterMethod, id=pk)
+
+    if request.method == "POST":
+        starter_step.delete()
+        messages.success(request, 'Step Deleted')
+        return HttpResponse("")
+
+    return HttpResponseNotAllowed(
+        [
+            "POST",
+        ]
+    )
+
+
+class StarterDelete(DeleteView):
+    """
+    Deletes Starter Course
+    """
+    model = Starter
+    success_url = '/starters/'
+
+    def test_func(self):
+
+        return self.request.user == self.get_object().user
+
+
+def starter_ing_details(request, pk):
+    """
+    Displays Ingredient Fields After Being Added
+    """
+    starter_ingredient = get_object_or_404(StarterIngredients, id=pk)
+    context = {
+        "starter_ingredient": starter_ingredient
+    }
+    return render(request, "includes/starter_ing_details.html", context)
+
+
+def starter_step_details(request, pk):
+    """
+    Displays Step Fields After Being Added
+    """
+    starter_step = get_object_or_404(StarterMethod, id=pk)
+    context = {
+        "starter_step": starter_step
+    }
+    return render(request, "includes/starter_step_details.html", context)
+
+
+def starter_ing_detail_view(request, pk):
+    """
+    Displays Ingredient Fields After Being Added
+    """
+    starter_ingredient = get_object_or_404(StarterIngredients, id=pk)
+    context = {
+        "starter_ingredient": starter_ingredient
+        }
+    return render(request, "includes/starter_ing_details.html", context)
+
+
+def starter_step_detail_view(request, pk):
+    """
+    Displays Ingredient Fields After Being Added
+    """
+    starter_step = get_object_or_404(StarterMethod, id=pk)
+    context = {
+        " starter_step":  starter_step
+        }
+    return render(request, "includes/ starter_step_details.html", context)
+
+
+def add_starter_ing(request):
+    """
+    Renders The Form Add Extra Ingredients
+    """
+    form = StarterIngredientForm()
+    context = {
+        "form": form
+    }
+    return render(request, "includes/add_starter_ing.html", context)
+
+
+def add_starter_step(request):
+    """
+    Renders The Form Add Extra Ingredients
+    """
+    form = StarterMethodForm()
+    context = {
+        "form": form
+    }
+    return render(request, "includes/add_starter_step.html", context)
