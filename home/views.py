06f89fec22a29e73529a2f1a6a5bd561c28d69a8
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-10-18 16:26:16 +0000

    Add Recipe view carousels to home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,23 @@
 from django.views.generic import TemplateView
+from main_courses.models import Main
+from starters.models import Starter
+from desserts.models import Dessert
+from django.shortcuts import render
 
-class Index(TemplateView):
-    """ Render home page """
-    template_name = 'home/index.html'
+
+
+def Index(request):
+    """
+    View full Main Recipie
+    """
+    main = Main.objects.all()
+    starter = Starter.objects.all()
+    dessert = Dessert.objects.all()
+
+    context = {
+        "main": main,
+        "starter": starter,
+        "dessert": dessert
+    }
+
+    return render(request, 'home/index.html', context,)
