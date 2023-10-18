726f9ee6d5c9d6ccbe436132f160ec4c918e324a
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-10-18 21:50:14 +0000

    Add index page layout with links to different sections

--- home/views.py
+++ home/views.py
@@ -2,8 +2,10 @@ from django.views.generic import TemplateView
 from main_courses.models import Main
 from starters.models import Starter
 from desserts.models import Dessert
+from sauces.models import Sauce
+from sides.models import Side
+from products.models import Product
 from django.shortcuts import render
-
 
 
 def Index(request):
@@ -13,11 +15,16 @@ def Index(request):
     main = Main.objects.all()
     starter = Starter.objects.all()
     dessert = Dessert.objects.all()
-
+    sauce = Sauce.objects.all()
+    side = Side.objects.all()
+    product = Product.objects.all()
     context = {
         "main": main,
         "starter": starter,
-        "dessert": dessert
+        "dessert": dessert,
+        "sauce": sauce,
+        "side": side,
+        "product":product
     }
 
     return render(request, 'home/index.html', context,)
